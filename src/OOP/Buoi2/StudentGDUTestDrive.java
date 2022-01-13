e5cdab59ae48d0794fd4b69d640e7a3b3a8227a7
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2022-01-13 16:51:54 +0700

    Up StudentGDU

--- src/OOP/Buoi2/StudentGDUTestDrive.java
+++ src/OOP/Buoi2/StudentGDUTestDrive.java
@@ -1,10 +1,9 @@
 package OOP.Buoi2;
 
 public class StudentGDUTestDrive {
-    public static void main(String[] args) {
         StudentGDU studentGDU;
         studentGDU = new StudentGDU();
-        
+
         studentGDU.nhapThongTin();
         studentGDU.hienThiThongTin();
         studentGDU.dangKyMonHoc();
