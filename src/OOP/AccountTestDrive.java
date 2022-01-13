5138d685905817e33dbbd07e44d860f52b20b329
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2022-01-13 13:58:44 +0700

    Up

---
+++ src/OOP/AccountTestDrive.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class AccountTestDrive {
+    Account account;
+
+    account = new Account();
+
+    account.show();
+    account.deposit();
+    account.show();
+
+    account.withdraw();
+    account.show();
+    
+}
