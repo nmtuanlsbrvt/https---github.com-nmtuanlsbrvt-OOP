7771721d0f40eaadd20634d2d7e8dd9f40b35bae
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2022-01-13 16:50:18 +0700

    Up StudentGDU

--- src/OOP/Buoi2/Sinhvien.java
+++ src/OOP/Buoi2/Sinhvien.java
@@ -12,7 +12,6 @@ public class Sinhvien {
         System.out.print("\tNhap id: ");
         id = sc.nextInt();
         System.out.print("\tNhap ten: ");
-        sc.nextLine();
         ten = sc.nextLine();
         System.out.print("\tNhap diem trung binh: ");
         diemTB = sc.nextDouble();
