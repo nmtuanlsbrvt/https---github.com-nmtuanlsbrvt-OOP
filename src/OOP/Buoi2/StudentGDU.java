ef19db7f8ed2a40f73bd2ddfe4a78205caeec131
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2022-01-18 12:15:38 +0700

    Update StudentGDU

--- src/OOP/Buoi2/StudentGDU.java
+++ src/OOP/Buoi2/StudentGDU.java
@@ -3,36 +3,16 @@ package OOP.Buoi2;
 import java.util.Scanner;
 
 public class StudentGDU {
-    Boolean gioitinh;
+    Boolean gioiTinh;
     int tuoi;
-    String Hovaten;
+    String name;
     String khoa;
     double diemTB ;
 
     Scanner sc = new Scanner(System.in);
 
-    void nhapThongTin() {
-        System.out.println("\tNhap gioi tinh: ");
-        gioitinh = sc.nextBoolean();
-        System.out.print("\tNhap tuoi: ");
-        tuoi= sc.nextInt();
-        System.out.print("\tNhap ten: ");
-        Hovaten = sc.nextLine();
-        System.out.print("\tNhap khoa: ");
-        khoa = sc.nextLine();
-        System.out.print("\tNhap diem trung binh: ");
-        diemTB = sc.nextDouble();
-    }
-    void hienThiThongTin() {
-        System.out.println("\tgioi tinh: "+ gioitinh);
-        System.out.println("\ttuoi: " + tuoi);
-        System.out.println("\tTen: " + Hovaten);
-        System.out.print("\tkhoa: "+ khoa);
-        System.out.println("\tDiem trung binh: " + diemTB);
-    }
-
-
     void hoc(){
+        System.out.println("học");
     };
     void thi(){
     };
--- src/OOP/Buoi2/StudentGDUTestDrive.java
+++ src/OOP/Buoi2/StudentGDUTestDrive.java
@@ -1,14 +1,18 @@
 package OOP.Buoi2;
 
 public class StudentGDUTestDrive {
-        StudentGDU studentGDU;
-        studentGDU = new StudentGDU();
+        public static void main(String[] args) {
+            StudentGDU tuanStudentGDU = new StudentGDU();
+            tuanStudentGDU.hoc();
 
-        studentGDU.nhapThongTin();
-        studentGDU.hienThiThongTin();
-        studentGDU.dangKyMonHoc();
-        studentGDU.thi();
-        studentGDU.hoc();
+            StudentGDU ducStudentGDU = new StudentGDU();
+            ducStudentGDU.thi();
+
+            StudentGDU xyzStudentGDU = new StudentGDU();
+
+            xyzStudentGDU.dangKyMonHoc();
+
+            
+
+        }
     }
-    
-}
