59d16da42c15a594e39cf1bff9238100badf8c42
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2022-01-18 12:25:54 +0700

    Update Shape

---
+++ src/OOP/Buoi2/ShapeTestDrive.java
@@ -0,0 +1,15 @@
+package OOP.Buoi2;
+
+public class ShapeTestDrive {
+    public static void main(String[] args) {
+        Shape triangle = new Shape();
+        triangle.xoay();
+
+        Shape circle = new Shape();
+        circle.xoay();
+        
+        Shape square = new Shape();
+        square.phatAmthanh();
+    }
+    
+}
