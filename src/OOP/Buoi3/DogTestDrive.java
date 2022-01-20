d0dc81b38a59bdea89c40b744038f90aee8cdabb
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2022-01-20 13:38:52 +0700

    Up Dog

---
+++ src/OOP/Buoi3/DogTestDrive.java
@@ -0,0 +1,12 @@
+package OOP.Buoi3;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog dog = new Dog(13, "Chiwawa", "Lulu");
+
+        dog.bark();
+        dog.setBigger();
+        dog.run();
+    
+    }
+}
