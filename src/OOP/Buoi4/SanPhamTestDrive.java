7ccd5143bcf7a997ac5802ca31b66477ff5d24fd
nmtuanlsbrvt <nmtuanlsbrvt@gmail.com>
2022-02-22 13:45:19 +0700

    Up Lab 4

---
+++ src/OOP/Buoi4/SanPhamTestDrive.java
@@ -0,0 +1,19 @@
+package OOP.Buoi4;
+
+public class SanPhamTestDrive {
+    public static void main(String[] args) {
+        SanPham sp = new SanPham();
+        SanPham sp1 = new SanPham();
+        SanPham sp2 = new SanPham();
+
+        sp.nhap();
+        sp.xuat();
+        sp.NhapThem();
+        sp.xuatThongTin();
+        sp1.inThongTin();
+        sp1.xuat();
+        sp2.xuat();
+        
+    }
+    
+}
